4662d38961604ddb32b07f353813374e38372a4c
Sienna <remicknsisi@gmail.com>
2023-03-11 10:32:57 -0500

    removed the macros that were unnecessary for review to chef relationship

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,4 +1,3 @@
 class Chef < ActiveRecord::Base
     has_many :recipes
-    has_many :reviews, through: :recipes
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,3 @@
 class Review < ActiveRecord::Base
     belongs_to :recipe
-    belongs_to :chef
 end
